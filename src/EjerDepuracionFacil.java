e2fb2b8c485405b6905864b930a4fb2a66579c40
anlodios <kiring@gmail.com>
2022-03-03 17:00:05 +0100

    Initial commit

---
+++ src/EjerDepuracionFacil.java
@@ -0,0 +1,20 @@
+
+public class EjerDepuracionFacil {
+
+    public static void main(String args[]){
+        double f=18.5;
+        double x=3.0;
+        for (int i=0; i<=100;i++){
+            if(i%2==0){
+                x++;
+                for(int j=70;j>0;j--){
+                    f=f+x/2;
+                }
+            }else{
+                x=x-f;
+            }
+        }
+
+
+    }
+}
