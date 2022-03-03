1191b9667d048f611b9bb08b07b01978e22046a3
anlodios <kiring@gmail.com>
2022-03-03 17:12:28 +0100

    modificado

--- src/EjerDepuracionFacil.java
+++ src/EjerDepuracionFacil.java
@@ -8,6 +8,7 @@ public class EjerDepuracionFacil {
             if(i%2==0){
                 x++;
                 for(int j=70;j>0;j--){
+                    if(f!=0)
                     f=f+x/2;
                 }
             }else{
